41465300cd4f9f57f9d5fdaf604a08b2f4267a82
Kofi korang <jbaah69@gmail.com>
2024-03-20 20:58:52 +0000

    file

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This module contains a procedure that calculates the fewest number of operations
+needed to result in exactly x H characters in the file.
+Prototype: `minOperations(x)`
+Returns: integer
+if x is impossible to achieve, returns 0
+"""
+
+
+def minOperations(x):
+    """
+    In a text file, there is a single character H.
+    Your text editor can execute only two operations in this file:
+    Copy All and Paste. write a method that calculates
+    the fewest number of operations needed to result in exactly x H characters
+    in the file
+    """
+
+    if x <= 1:
+        return 0
+    num, div, numOfOperations = x, 2, 0
+
+    while num > 1:
+        if num % div == 0:
+            num = num / div
+            numOfOperations = numOfOperations + div
+        else:
+            div += 1
+    return numOfOperations
