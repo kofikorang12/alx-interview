c2031a4ce19a2b09323c133fec37e921cbf6b364
kofikorang12 <jbaah69@gmail.com>
2024-03-28 14:57:51 +0000

    a script that reads stdin line by line and computes metrics

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,10 +2,9 @@
 
 import sys
 
-
 def print_msg(dict_sc, total_file_size):
     """
-    Method to print
+    Methds to print
     Args:
         dict_sc: dict of status codes
         total_file_size: total of the file
